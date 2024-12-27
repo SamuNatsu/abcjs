38ccc46a33022f1f34d4342142b0396198dc50b4
SNRainiar <rainiar@foxmail.com>
2024-12-28 00:01:43 +0800

    feat: add i18n entry and sidebar for zh-cn

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,28 @@ export default {
 	theme: defaultTheme({
 		logo: '/img/abcjs_comp_extended_08.svg',
 		displayAllHeaders: true,
-		sidebar: require("./sidebar")
+		sidebar: require("./sidebar"),
+		locales: {
+			'/': {},
+			'/zh-cn/': {
+				selectLanguageText: '选择语言',
+				selectLanguageName: '简体中文',
+				sidebar: require("./sidebar.zh-cn.json")
+			}
+		}
 	}),
 	head: [
 		['link', { rel: 'icon', href: '/abcjs/favicon.ico' }],
 		['link', { rel:"stylesheet", type: "text/css", href: 'https://paulrosen.github.io/abcjs/abcjs-audio.css'}],
 		['script', { src: "https://paulrosen.github.io/abcjs/dist/abcjs-basic.js", type:"text/javascript" }]
 	],
+	locales: {
+		'/': {
+			lang: 'en-US'
+		},
+		'/zh-cn/': {
+			lang: 'zh-CN',
+			description: '用于在浏览器中展示乐谱的 JavaScript 库'
+		}
+	}
 };
